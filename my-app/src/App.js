b99d76eb47d93b5fec1d5484588b0c640c315234
Igor Kuzevanov <kuzevigo@fel.cvut.cz>
2018-10-18 22:06:14 +0200

    Yasqee component added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Yasqe from './Yasqe'
+
 
 class App extends Component {
     state = {
@@ -8,35 +9,16 @@ class App extends Component {
         users: []
     };
 
-    async componentDidMount() {
-        const response = await fetch('/index');
-        const body = await response.json();
-        console.log(body);
-        this.setState({ users: body, isLoading: false });
-    }
+    // async componentDidMount() {
+    //     const response = await fetch('/index');
+    //     const body = await response.json();
+    //     console.log(body);
+    //     this.setState({users: body, isLoading: false});
+    // }
 
     render() {
-        const {users, isLoading} = this.state;
-
-        if (isLoading) {
-            return <p>Loading...</p>;
-        }
-
         return (
-            <div className="App">
-              <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <h1 className="App-title">Welcome to React</h1>
-              </header>
-              <div className="App-intro">
-                <h2>JUG List</h2>
-                  {users.map(user =>
-                      <div key={user.id}>
-                          {user.firstName} {user.lastName}
-                      </div>
-                  )}
-              </div>
-            </div>
+            <Yasqe/>
         );
     }
 }
